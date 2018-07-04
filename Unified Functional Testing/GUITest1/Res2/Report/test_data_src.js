de4307ce311b19518764b315270bd0ca34e490d0
SmlCodes <smlcodes@gmail.com>
2018-07-04 06:16:02 +0000

    UFT GUI TEST

---
+++ Unified Functional Testing/GUITest1/Res2/Report/test_data_src.js
@@ -0,0 +1,1 @@
+﻿var __testDataSource={items:[{dataFilePath:'Default.xls',dataFileFormat:'excel',dataFileFolder:'C:\\Users\\winadmin\\Documents\\Unified Functional Testing\\GUITest1\\Res2',namePairs:[]}]};
